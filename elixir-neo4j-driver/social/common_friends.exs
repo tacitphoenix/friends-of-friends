0d85cb9be2f4ebf5b0f4ca0d11ffc6845cc71d7b
tacitphoenix <sondiek@gmail.com>
2019-11-07 10:30:18 -0500

    use parameters with cyper queries

--- elixir-neo4j-driver/social/common_friends.exs
+++ elixir-neo4j-driver/social/common_friends.exs
@@ -1,13 +1,13 @@
-[[{'username', username}, {'password', password}, {'host', host}, _ , _]] = :yamerl_constr.file("../../config.yml")
+[[{'userName', username}, {'password', password}, {'host', host}, _ , _, _]] = :yamerl_constr.file("../../config.yml")
 {:ok, _neo} = Bolt.Sips.start_link(url: "bolt://#{username}:#{password}@#{host}")
 conn = Bolt.Sips.conn()
 
 # common friends
-{user, foaf} = {"Joe", "Sally"}
+params = %{user: "Joe", foaf: "Sally"}
 common_friends_query = """
   MATCH (user:Person)-[:KNOWS]-(friend)-[:KNOWS]-(foaf:Person)
-  WHERE user.name = \"#{user}\" AND foaf.name = \"#{foaf}\"
+  WHERE user.name = {user} AND foaf.name = {foaf}
   RETURN friend.name AS friend
 """
 
-Bolt.Sips.query!(conn, common_friends_query) |> IO.inspect
+Bolt.Sips.query!(conn, common_friends_query, params) |> IO.inspect

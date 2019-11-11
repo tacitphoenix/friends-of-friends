272a11613426d9f1cedea9389ba7c170a731376e
tacitphoenix <sondiek@gmail.com>
2019-11-10 22:40:20 -0500

    refactor class and add tests

--- java-neo4j-driver/social/src/main/java/com/tacitphoenix/neo4j/Neo4jConfigReader.java
+++ java-neo4j-driver/social/src/main/java/com/tacitphoenix/neo4j/Neo4jConfigReader.java
@@ -3,13 +3,22 @@ package com.tacitphoenix.neo4j;
 import org.yaml.snakeyaml.Yaml;
 import org.yaml.snakeyaml.constructor.Constructor;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.InputStream;
 
 public class Neo4jConfigReader {
     public static Neo4jConfig get() {
-        Yaml yaml = new Yaml(new Constructor(Neo4jConfig.class));
-        InputStream inputStream = Neo4jConfigReader.class.getClassLoader().getResourceAsStream("config.yml");
-        Neo4jConfig config = yaml.load(inputStream);
+        Neo4jConfig config = null;
+        try {
+            Yaml yaml = new Yaml(new Constructor(Neo4jConfig.class));
+            File file = new File(Neo4jConfigReader.class.getClassLoader().getResource("config.yml").getFile());
+            InputStream inputStream = new FileInputStream(file);
+            config = yaml.load(inputStream);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
         return config;
     }
 }
